59e3c03dcd30295cecdd95d5de8e233a5993b9b0
agentoflyra <agentoflyra@gmail.com>
2026-03-04 14:23:32 +0800

    ruff check

--- launch_ROS2/rviz_MID360_launch.py
+++ launch_ROS2/rviz_MID360_launch.py
@@ -1,8 +1,6 @@
 import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-import launch
 
 ################### user configure parameters for ros2 start ###################
 xfer_format   = 0    # 0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format
